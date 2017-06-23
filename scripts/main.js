bc6b99a6984d42157ef05f9769546d5f87e17e80
AlexChi <alexchi1114@gmail.com>
2017-06-23 16:07:05 -0400

    calculator finished

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,44 @@
+function add(num1,num2){
+	return num1+num2;
+}
+
+function multiply(num1,num2){
+	return num1*num2;
+}
+
+function divide(num1,num2){
+	return num1/num2;
+}
+
+function subtract(num1,num2){
+	return num1-num2;
+}
+
+function calculation(string){
+
+}
+
+
 $(document).ready(function(){
-	$('.test').click(function(){
-		$(this).hide();
+
+	$display_text = ''
+
+	$('#equal').click(function(){
+		$display_text = eval($display_text);
+		$('#display').text($display_text);
 	})
+
+	$('.button').click(function(){
+		$display_text += $(this).text();
+		$('#display').text($display_text);
+	})
+
+
+
+	$('#clear').click(function(){
+		$display_text = ''
+		$('#display').text($display_text);
+	})
+
+
 })
